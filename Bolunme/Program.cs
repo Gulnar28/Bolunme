6a1f36c96c7dd3f3862ffb7c784fc19c17dd9615
gulnarvma@outlook.com <gulnarvma@outlook.com>
2022-12-11 12:27:37 +0400

    first commit

---
+++ Bolunme/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Bolunme
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int bolunme = a % b;
+            Console.WriteLine($"{a}%{b}={bolunme}");
+            ;
+        }
+    }
+}
